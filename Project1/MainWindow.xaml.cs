4fde1ae06d191543672cbb98e9f79cf4382d3261
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-09-24 14:18:58 -0500

    initial commit

---
+++ Project1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show("Note: Please make sure to input a payment that exceeds the amount of interest to be paid. Otherwise, you will not be making progress towards paying off your principle.");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double payment, balance, interestRate, principle, interest;
+            String alert = "";
+
+            payment = Double.Parse(monthlyPayment.Text);
+            balance = Double.Parse(outstandingBalance.Text);
+            interestRate = 0.0639;
+
+            // determine the interest portion
+            interest = (balance * interestRate) / 12;
+
+            // determine the principle portion
+            principle = payment - interest;
+
+            // cover the case where the payment is too low
+            if (interest >= payment)
+            {
+                interest = payment;
+                principle = 0;
+                alert = "Please increase your payment to avoid underpaying!";
+            }
+
+            // display breakdown
+            MessageBox.Show("Your payment breakdown is: \nPrinciple: "
+                + principle.ToString("C", CultureInfo.CurrentCulture) + "\nInterest: " 
+                + interest.ToString("C", CultureInfo.CurrentCulture) + "\n" + alert);
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int price = Int32.Parse(Console.ReadLine());
+            int payment = 100;
+            int quarters, nickels, dimes, change;
+
+            // Welcome Message
+            Console.WriteLine("Enter the price of the item: ", price);
+
+            // Make sure price is valid
+            if (price % 5 != 0 || price < 25)
+            {
+                Console.WriteLine("Invalid price. Please try again.");
+                Console.WriteLine("Enter the price of the item: ", price);
+            }
+
+            // Determine Change
+            change = payment - price;
+
+            // Determine number of quarters
+            quarters = change % 25;
+            change -= quarters * 25;
+
+            // Determine number of dimes
+            dimes = change % 10;
+            change -= dimes * 10;
+
+            // Determine number of nickels
+            nickels = change % 5;
+
+            // Display change breakdown 
+            Console.WriteLine("You bought an item for " + price +
+                "cents and gave me a dollar. Your change is: \n" +
+                quarters + " quarters\n" + dimes + " dimes, and\n" +
+                nickels + " nickels.");
+        }
+    }
+}
